36a7628761ea7f54d4be54e1999d592826659faa
Elisey Esaulkov <spbox@yahoo.com>
2022-04-19 13:03:46 +0500

    Triangle 1 simple and Triangle 2 cursor to a specific position

--- StudyProject/Program.cs
+++ StudyProject/Program.cs
@@ -10,10 +10,51 @@ namespace StudyProject
     {
         static void Main(string[] args)
         {
+            #region String code
             //string a = "My";
             //string b = "study work";
             //Console.WriteLine("1: {0} {1}"  , a , b);
             //Console.ReadKey();
+            #endregion String code
+
+            #region Triangle 1 simple
+            //string symbol = "*";
+
+            //Console.Write("Введите высоту треугольника: ");
+            //int hight = int.Parse(Console.ReadLine());
+
+            //for (int currentRow = 0; currentRow < hight; currentRow++)
+            //{
+            //    for (int symbolFillengLine = hight; symbolFillengLine > currentRow; symbolFillengLine--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int symbolFillengLine = 0; symbolFillengLine <= currentRow; symbolFillengLine++)
+            //    {
+            //        Console.Write(symbol);
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+            #endregion Triangle 1 simple
+
+            #region Triangle 2 cursor to a specific position
+            string symbol = "* ";  // Пробел добавлено, так как смещать мы можем только на дискретный шаг символа
+
+            Console.Write("Введите высоту треугольника: ");
+            int hight = int.Parse(Console.ReadLine());
+
+            for (int currentRow = 0; currentRow < hight; currentRow++)
+            {
+                Console.SetCursorPosition((Console.WindowWidth / 2 - currentRow), currentRow + 2);       // Смещать мы можем только на дискретный шаг символа
+                for (int symbolFillengLine = 0; symbolFillengLine <= currentRow; symbolFillengLine++)
+                {
+                    Console.Write(symbol);
+                }
+            }
+            Console.ReadKey();
+            #endregion Triangle 2 cursor to a specific position
 
 
 
